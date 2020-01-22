d46009e668cdf03d53c93875afa653a96a9c03cf
Diego Lottermann <diegolottermann@gmail.com>
2020-01-22 20:49:41 -0300

    Fix some validations on inputs

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,41 +2,71 @@ const Chromy = require('chromy')
 
 module.exports = (app) => {
     app.get('/', (req, res, next) => { res.send('Page Not Found'); next(); })
-    app.post('/buscar', (req, res,next) => {
-        
-       main(req.body.checkin,req.body.checkout)
-        .then((e) => {
-          res.send(e)
-          next();
-        }).catch(err => console.log(err))
-     
+    app.post('/buscar', (req, res, next) => {
+
+
+
+        if (Object.keys(req.body).length === 0) {
+            res.send({
+                title: 'Erro',
+                message: 'Informe as datas de check-in e check-out corretamente'
+            })
+        } else {
+            let checkin = parseInt(req.body.checkin)
+            let checkout = parseInt(req.body.checkout)
+
+            if (checkin > checkout) {
+                res.send({
+                    title: 'Erro',
+                    message: 'Informe as datas de check-in e check-out corretamente - Check-out deve ser maior que o check-in'
+                })
+            } else if (checkin === checkout) {
+                res.send({
+                    title: 'Erro',
+                    message: 'Informe as datas de check-in e check-out corretamente - Data iguais não são permitidas'
+                })
+            } else {
+                main(req.body.checkin, req.body.checkout)
+                    .then((e) => {
+                        res.send(e)
+                        next();
+                    }).catch(err => console.log(err))
+            }
+
+
+        }
+
+
+
+
+
     })
 }
 
-  //async function
-  async function main (checkin,checkout) {
+//async function
+async function main(checkin, checkout) {
     let chromy = new Chromy()
     await chromy.goto(`https://myreservations.omnibees.com/default.aspx?q=5462&version=MyReservation&sid=c1946547-5754-4e23-b840-bd6a0947765f#/&diff=false&CheckIn=${checkin}&CheckOut=${checkout}&Code=&group_code=&loyality_card=&NRooms=1&ad=1&ch=0&ag=-`)
     const result = await chromy.evaluate(() => {
 
-        
-          const hoteis = []
-           //scrap loop of elements                
-          document.querySelectorAll('.roomExcerpt').forEach(item => {
-             
-            item.querySelectorAll('h5 > a').forEach(hotel => hoteis.push({'title':hotel.textContent}))
-            item.querySelectorAll('.sincePriceContent > h6').forEach(hotel => hoteis.push({'price':hotel.textContent}))
-            item.querySelectorAll('a.description').forEach(hotel => hoteis.push({'description':hotel.textContent}))
-            item.querySelectorAll('.slide > a').forEach(hotel => hoteis.push({'pictures':hotel.href}))
-            item.querySelectorAll('.slide > a > img').forEach(hotel => hoteis.push({'thumbnail':hotel.src}))
-         
-        })
-      
-          return hoteis
 
-          })
-          
-    
+        const hoteis = []
+        //scrap loop of elements                
+        document.querySelectorAll('.roomExcerpt').forEach(item => {
+
+            item.querySelectorAll('h5 > a').forEach(hotel => hoteis.push({ 'title': hotel.textContent }))
+            item.querySelectorAll('.sincePriceContent > h6').forEach(hotel => hoteis.push({ 'price': hotel.textContent }))
+            item.querySelectorAll('a.description').forEach(hotel => hoteis.push({ 'description': hotel.textContent }))
+            item.querySelectorAll('.slide > a').forEach(hotel => hoteis.push({ 'pictures': hotel.href }))
+            item.querySelectorAll('.slide > a > img').forEach(hotel => hoteis.push({ 'thumbnail': hotel.src }))
+
+        })
+
+        return hoteis
+
+    })
+
+
     await chromy.close()
     return result
-  }
+}
